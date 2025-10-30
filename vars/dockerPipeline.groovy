9fdd8cdbf64fa7f267f0126a6cffc17520f8e052
Camila <cmlavaut96@gmail.com>
2025-10-29 22:19:48 -0600

    Final test5

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,35 +1,29 @@
 def call(Map config) {
-    pipeline {
-        agent any
-        stages {
-
-            stage('Login Docker Hub') {
-                steps {
-                    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+    stage('Login Docker Hub') {
+        steps {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
                         sh "echo $PASS | docker login -u $USER --password-stdin"
-                    }
-                }
-            }
-
-            stage('Push Image') {
-                steps {
-                    sh "docker push ${config.imageName}"
-                }
-            }
-
-            stage('Trigger Deploy Pipeline') {
-                when { expression { return config.triggerDeploy } }
-                steps {
-                    script {
-                        def branch = env.BRANCH_NAME
-                        if (branch == "master") {
-                            build job: "Deploy_to_master", parameters: [string(name: 'IMAGE_NAME', value: config.imageName)]
-                        } else if (branch == "dev") {
-                            build job: "Deploy_to_dev", parameters: [string(name: 'IMAGE_NAME', value: config.imageName)]
-                        }
-                    }
-                }
             }
         }
+    }
+    stage('Push Image') {
+        steps {
+                    sh "docker push ${config.imageName}"
+        }
+    }
+    
+    
+    stage('Trigger Deploy Pipeline') {
+        when { expression { return config.triggerDeploy } }
+            steps {
+                 script {
+                    def branch = env.BRANCH_NAME
+                    if (branch == "master") {
+                        build job: "Deploy_to_master", parameters: [string(name: 'IMAGE_NAME', value: config.imageName)]
+                    } else if (branch == "dev") {
+                        build job: "Deploy_to_dev", parameters: [string(name: 'IMAGE_NAME', value: config.imageName)]
+                    }
+                }
+            }
     }
 }
