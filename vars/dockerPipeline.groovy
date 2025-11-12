de554bcf4589c6b9ef7eddb287077c4883473505
Camila <cmlavaut96@gmail.com>
2025-11-12 16:34:15 -0600

    test con try and catch error

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -3,26 +3,42 @@ def call(Map config = [:]) {
     def triggerDeploy = config.get('triggerDeploy', false)
     def extraArgs = config.get('extraArgs', '') 
 
+    echo "Iniciando Pipeline para image: ${image_dock}"
+
     stage('Login Docker Hub') {
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                        sh "echo $PASS | docker login -u $USER --password-stdin"
+        try {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+             sh "echo $PASS | docker login -u $USER --password-stdin"
+            }
         }
-        
+        catch (err) {
+            echo "Error: ${err}"
+        }        
     }
+
     stage('Push Image') {
-        sh "docker push ${image_dock}"
-        echo "Imagen en el DockerHub"
+        try {
+            sh "docker push ${image_dock}"
+            echo "Imagen en el DockerHub"
+        }
+        catch (err){
+            echo "error en la imagen enviada: ${err}"
+        }
         
     }
     
     if (triggerDeploy) {
         stage('Trigger Deploy Pipeline') {
-            echo "Deploying container from image: ${image_dock}"
-            sh """
-            docker stop ${image_dock} || true
-            docker rm ${image_dock} || true
-            docker run -d --name ${image_dock} ${extraArgs} ${image_dock}
-            """
+            try {
+                echo "Deploying container from image: ${image_dock}"
+                sh """
+                docker stop ${image_dock} || true
+                docker rm ${image_dock} || true
+                docker run -d --name ${image_dock} ${extraArgs} ${image_dock}
+                """  
+            } catch (err) {
+                echo "Error en el despliegue: ${err}"
+            }
         }
     }
 }
